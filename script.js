ebb607b5763dbd419dd0ef46eb328244f4075e1a
keshav kumar <keshavks9810@gmail.com>
2024-03-02 10:58:13 +0530

    lec-1 practice code

---
+++ script.js
@@ -0,0 +1,14 @@
+const heading=
+React.createElement("div",
+{Id:"parent"},
+["Hello world!",React.createElement("div",
+    {Id:"child"},
+    ["Hello world!",React.createElement("h3",
+    {},
+    "Hello world!")])])
+
+
+
+
+
+ReactDOM.createRoot(document.getElementById("div1")).render(heading);
